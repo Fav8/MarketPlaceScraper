caead89ada686858f5bbacb55b3e730bbcbf0069
Fav8 <leonardo.damico7597@gmail.com>
2022-05-27 09:53:24 +0200

    removed testing logs

--- export.js
+++ export.js
@@ -3,8 +3,7 @@ const { filterPipe } = require('./filter.js');
 
 async function bootstrap(){
   let csvData = []
-/*   const iphone12 = await filterPipe('iphone','12'); */
-  console.log(process.argv[2])
+const iphone12 = await filterPipe('iphone','12');
 /*   if(process.argv[3] === 'iphone'){
   const iphone11 = await filterPipe('iphone','11');
   const iphone12 = await filterPipe('iphone','12');
@@ -12,8 +11,8 @@ async function bootstrap(){
   const iphonexr = await filterPipe('iphone','xr');
   csvData.push(iphone11,iphone12,iphone13,iphonexr)
   } */
-/*   const csv = new ObjectsToCsv(iphone12);
-  await csv.toDisk('./test.csv'); */
+const csv = new ObjectsToCsv(iphone12);
+  await csv.toDisk('./test.csv');
 }
 
 bootstrap()
